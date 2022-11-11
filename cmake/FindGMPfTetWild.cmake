2f1e4f909f37a143b3897c0e4c7b40c99443ef7d
Simone <simone87.mandelli@gmail.com>
2022-11-11 16:54:22 +0100

    Fix cmakeList - commenting topLevel build

--- cmake/FindGMPfTetWild.cmake
+++ cmake/FindGMPfTetWild.cmake
@@ -3,35 +3,28 @@
 # GMP_INCLUDE_DIRS - the GMP include directory
 # GMP_LIBRARIES - Libraries needed to use GMP
 
-#if (GMP_INCLUDE_DIRS AND GMP_LIBRARIES)
-        # Already in cache, be silent
-#        set(GMP_FIND_QUIETLY TRUE)
-#endif (GMP_INCLUDE_DIRS AND GMP_LIBRARIES)
+#if (WIN32)
+  MESSAGE(STATUS "GMP environment variables: " $ENV{GMP_INC} " " $ENV{GMP_LIB} )
+  find_path(GMP_INCLUDE_DIRS NAMES mpir.h PATHS $ENV{GMP_INC} ${GMP_WINDOWS_PATH})
+  find_library(GMP_LIBRARIES NAMES mpir PATHS $ENV{GMP_LIB} ${GMP_WINDOWS_PATH})
+  MESSAGE(STATUS "GMP libs: " ${GMP_LIBRARIES} " " ${GMPXX_LIBRARIES} )
+  include(FindPackageHandleStandardArgs)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP DEFAULT_MSG GMP_INCLUDE_DIRS GMP_LIBRARIES)
+  mark_as_advanced(GMP_INCLUDE_DIRS GMP_LIBRARIES)
+  MESSAGE(STATUS "GMP libs: " ${GMP_LIBRARIES} " " ${GMP_INCLUDE_DIRS} )
+#else()
+ # if (GMP_INCLUDE_DIRS AND GMP_LIBRARIES)
+    # Already in cache, be silent
+#    set(GMP_FIND_QUIETLY TRUE)
+#  endif (GMP_INCLUDE_DIRS AND GMP_LIBRARIES)
+#  find_path(GMP_INCLUDE_DIRS NAMES gmp.h PATHS $ENV{GMP_INC} ${GMP_WINDOWS_PATH})
+#  find_library(GMP_LIBRARIES NAMES gmp libgmp PATHS $ENV{GMP_LIB} ${GMP_WINDOWS_PATH})
+#  find_library(GMPXX_LIBRARIES NAMES gmpxx libgmpxx PATHS $ENV{GMP_LIB} ${GMP_WINDOWS_PATH})
+  #MESSAGE(STATUS "GMP libs: " ${GMP_LIBRARIES} " " ${GMPXX_LIBRARIES} )
 
+#  include(FindPackageHandleStandardArgs)
+#  FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP DEFAULT_MSG GMP_INCLUDE_DIRS GMP_LIBRARIES)
 
-
-#if(WIN32)
-#	if(CYGWIN)
-#		triwild_download_gmp_cygwin()
-	#elseif(MINGW)
-#		triwild_download_gmp_mingw()
-	#else()
-#		triwild_download_gmp_vc()
-	#endif()
-
-	#SET(GMP_WINDOWS_PATH ${THIRD_PARTY_DIR}/gmp)
+#  mark_as_advanced(GMP_INCLUDE_DIRS GMP_LIBRARIES)
+#  MESSAGE(STATUS "GMP libs: " ${GMP_LIBRARIES} " " ${GMP_INCLUDE_DIRS} )
 #endif()
-
-MESSAGE(STATUS "GMP environment variables: " $ENV{GMP_INC} " " $ENV{GMP_LIB} )
-#find_path(GMP_INCLUDE_DIRS NAMES gmp.h PATHS $ENV{GMP_INC} ${GMP_WINDOWS_PATH})
-#find_library(GMP_LIBRARIES NAMES gmp libgmp PATHS $ENV{GMP_LIB} ${GMP_WINDOWS_PATH})
-#find_library(GMPXX_LIBRARIES NAMES gmpxx libgmpxx PATHS $ENV{GMP_LIB} ${GMP_WINDOWS_PATH})
-find_path(GMP_INCLUDE_DIRS NAMES mpir.h PATHS $ENV{GMP_INC} ${GMP_WINDOWS_PATH})
-find_library(GMP_LIBRARIES NAMES mpir PATHS $ENV{GMP_LIB} ${GMP_WINDOWS_PATH})
-MESSAGE(STATUS "GMP libs: " ${GMP_LIBRARIES} " " ${GMPXX_LIBRARIES} )
-
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP DEFAULT_MSG GMP_INCLUDE_DIRS GMP_LIBRARIES)
-
-mark_as_advanced(GMP_INCLUDE_DIRS GMP_LIBRARIES)
-MESSAGE(STATUS "GMP libs: " ${GMP_LIBRARIES} " " ${GMP_INCLUDE_DIRS} )
